b291461011beebf6d74389005c491294f2cb8df4
Olivier Cardinaux <ocardinaux@gmail.com>
2022-04-01 17:41:41 +0200

    refactor: prettify

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 #!/usr/bin/env node
 
-import "dotenv/config"
+import "dotenv/config";
 import * as library from "./lib/library.js";
 import chalk from "chalk";
-import { Command } from 'commander';
+import { Command } from "commander";
 
 const program = new Command();
 
-program
-  .name("mynot")
-  .description("My Notion CLI applications")
-  .version()
+program.name("mynot").description("My Notion CLI applications").version();
 
 program
   .command("addnewbooks")
-  .description(
-    "get the new books in calibre"
-  )
-  .action(async() => {
-    await library.addNewBooks()
+  .description("get the new books in calibre")
+  .action(async () => {
+    await library.addNewBooks();
   });
 
 // BUG: it looks like there are some pending promises somewhere
 program
   .command("lastbook")
-  .description(
-    "gets the idx of the last book imported in notion"
-  )
-  .action( async () => {
-    let res = await library.lastBook();
+  .description("gets the idx of the last book imported in notion")
+  .action(async () => {
+    const res = await library.lastBook();
     console.log(chalk.green(`Last index is ${res}`));
   });
 
 program.parseAsync().then(() => {
-  console.log('done.');
+  console.log("done.");
 });
